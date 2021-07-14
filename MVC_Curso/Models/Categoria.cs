78d8063430678ac511a482f151e859c9194ab4ee
Mateus Goncalves <mateusgsbh2@gmail.com>
2021-07-14 18:10:28 -0300

    first commit

---
+++ MVC_Curso/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Curso.Models
+{
+    [Keyless]
+    public class Categoria
+    {
+        private int Id { get; set; }
+        private string Descricao { get; set; }
+    }
+}
---
+++ MVC_Curso/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Curso.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer(connectionString: "Server=DESKTOP-C6OAH2D/SQLEXPRESS;Database=CursoMVC;Trusted_Connection=True");
+        }
+
+    }
+}
